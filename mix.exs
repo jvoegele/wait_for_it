f31fa942ace85a5b64be574da0ee7df6cb1449d4
Jason Voegele <jason.voegele@gmail.com>
2018-02-07 16:34:18 -0500

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -25,7 +25,8 @@ defmodule WaitForIt.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.16", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.18", only: :dev, runtime: false},
+      {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false}
     ]
   end
 
