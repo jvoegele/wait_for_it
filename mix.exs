16aacf793377038a66cf9e0824e04a4140269fae
Jason Voegele <jason.voegele@gmail.com>
2019-03-08 10:49:26 -0500

    Increment version to 1.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WaitForIt.Mixfile do
   def project do
     [
       app: :wait_for_it,
-      version: "1.1.1",
+      version: "1.2.0",
       description: "Elixir library for waiting for things to happen",
       source_url: "https://github.com/jvoegele/wait_for_it",
       elixir: "~> 1.5",
