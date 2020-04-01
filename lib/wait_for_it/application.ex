b16cb62c6991f3d7c4200ffa17c0c9ddf4f596be
Jason Voegele <jason.voegele@gmail.com>
2020-04-01 15:25:17 -0400

    Use child specs instead of deprecated Supervisor.Spec

--- lib/wait_for_it/application.ex
+++ lib/wait_for_it/application.ex
@@ -2,15 +2,13 @@ defmodule WaitForIt.Application do
   @moduledoc false
 
   use Application
-  import Supervisor.Spec, warn: false
 
   def start(_type, _args) do
     children = [
-      supervisor(Registry, [:unique, WaitForIt.ConditionVariable.registry()]),
-      supervisor(WaitForIt.ConditionVariable.Supervisor, [])
+      {Registry, keys: :unique, name: WaitForIt.ConditionVariable.registry()},
+      WaitForIt.ConditionVariable.Supervisor
     ]
 
-    opts = [strategy: :one_for_one, name: WaitForIt.Supervisor]
-    Supervisor.start_link(children, opts)
+    Supervisor.start_link(children, strategy: :one_for_one, name: WaitForIt.Supervisor)
   end
 end
--- lib/wait_for_it/condition_variable/supervisor.ex
+++ lib/wait_for_it/condition_variable/supervisor.ex
@@ -1,7 +1,7 @@
 defmodule WaitForIt.ConditionVariable.Supervisor do
   use Supervisor
 
-  def start_link do
+  def start_link(_) do
     Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
